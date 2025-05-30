9b322115ed72ea75fcba1b48ac70913faef2c7dd
AlbertooPadeiro <lucaslevy3248@gmail.com>
2025-05-29 20:46:41 -0400

    I actually don't know what I'm doing

--- src/Audio/Audio.h
+++ src/Audio/Audio.h
@@ -1,20 +1,24 @@
 #ifndef AUDIOBUS_H
 #define AUDIOBUS_H
-#include <spa/utils/hook.h>
-
 
 
 class Audio {
-private:
-  struct pw_main_loop *loop;
-  struct pw_context *context;
-  struct pw_core *core;
-  struct pw_registry *registry;
-  struct spa_hook registry_listener;
+public:
+
+  struct data {
+    struct pw_main_loop *loop;
+    struct pw_context *context;
+    struct pw_core *core;
+
+  };
+  // struct pw_main_loop *loop;
+  // struct pw_context *context;
+  // struct pw_core *core;
+  // struct pw_registry *registry;
+  // struct spa_hook registry_listener;
 public:
   Audio();
   ~Audio();
-   void createDevice(bool isInput, bool isVirtual);
 };
 
 
--- src/Audio/Mixer.cpp
+++
@@ -1,2 +0,0 @@
-
-#include "Mixer.h"
